061a4dfbdfba756f52f3c0d2ed64164cdc27f873
VanekWTF <minemaster55yt@mail.ru>
2024-10-08 02:04:21 +0400

    Obnova

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import MainMenu from './components/MainMenu/MainMenu';
+import Canvas from './components/Canvas';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <MainMenu/>
+      <Canvas/>
     </div>
   );
 }
--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./MainMenu.css";
 
 const MainMenu: React.FC = () => {
-    return (<>PIVO</>);
+    return (
+    <body className="Menu">
+        <p>
+            ГЕРОЙ
+        </p>
+        <p>
+            МОНЕТКИ
+        </p>
+        <p>
+            ЭНЕРГИЯ
+        </p>
+        <p>
+            КАРТА
+        </p>
+        <p>
+            ПОКЕМОНЫ
+        </p>
+        <p>
+            НАСТРОЙКИ
+        </p>
+        <p>
+            РЫНОК
+        </p>
+    </body>
+);
 };
 
 export default MainMenu;
